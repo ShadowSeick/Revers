49c5764c372c6f5a6fb54693070e338465ba9b8c
Adrián Muñoz González <alcolka@gmail.com>
2021-07-25 13:41:59 +0200

    Aplication finalized, All works perfectly well without bugs.

--- js/site.js
+++ js/site.js
@@ -1,17 +1,36 @@
-//start function
+sh//start function
 function getText() {
 
-    let textToReverse = document.getElementById("submitedText").nvalue;
+    let textToReverse = document.getElementById("submitedText").value;
     textToReverse = textToReverse.toString();
-    alert("This is your text" + textToReverse);
-}
+    
+    //Check if there is any text.
+    if (textToReverse) {
+
+        //Call reverseText
+        let textReversed = reverseText(textToReverse);
+
+        //Call displayText
+        displayText(textReversed);
+    }
+    else {
+        alert("You have to put text in it")
+    }
+ }
 
 //logic function
-function reverseText() {
-
+function reverseText(text) {
+    return text.split("").reverse().join("");
 }
 
 //display function
-function displayReverse() {
+function displayText(text) {
 
+    let placeToDisplay = document.getElementById("result");
+    
+    if (placeToDisplay.innerHTML){
+        placeToDisplay.innerHTML = "";
+    }
+
+    placeToDisplay.innerHTML += `This is your text reversed: <span class="fw-bold fs-4">${text}</span>`;
 }
